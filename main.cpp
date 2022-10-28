94956d8c28a56f3582423fc3f7beb2ea41c9d20e
Nicolas Russo <nicolasrusso291@gmail.com>
2022-10-28 00:39:53 +0000

    v2

--- main.cpp
+++ main.cpp
@@ -16,13 +16,24 @@ private:
 };
 
 DigitalOut led1(LED1);
+DigitalOut led3(LED3);
 Flipper f(LED2);
 Ticker t;
+Ticker flipper;
+
+void flip()
+{
+    led3 = !led3;
+}
+
+
 
 int main()
 {
     // the address of the object, member function, and interval
     t.attach(callback(&f, &Flipper::flip), 2.0);
+    led3 = 1;
+    flipper.attach(&flip, 2.0); // the address of the function to be attached (flip) and the interval (2 seconds)
 
     // spin in a main loop. flipper will interrupt it to call flip
     while (1) {
